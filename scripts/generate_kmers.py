d7ef3ec52e653e3b25beb918a9aede911970b916
Henri Schmidt <packerfan73@gmail.com>
2022-02-01 09:25:55 -0500

    Create generate_kmers python script.

---
+++ scripts/generate_kmers.py
@@ -0,0 +1,111 @@
+from Bio import SeqIO
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Generates a set of kmers for processing by Guidescan2."
+    )
+
+    parser.add_argument(
+        "fasta",
+        type=str,
+        help="FASTA file to use as a reference for kmer generation."
+    )
+
+    parser.add_argument(
+        "--pam",
+        help="Length of kmers to generate.",
+        default="NGG"
+    )
+
+    parser.add_argument(
+        "--kmer-length",
+        help="Length of kmers to generate.",
+        default=20
+    )
+
+    parser.add_argument(
+        "--min-chr-length",
+        help="Minimum chromosome length to consider for kmer generation.",
+        default=10_000
+    )
+
+    parser.add_argument(
+        "--prefix",
+        help="Prefix to use for kmer identifiers.",
+        default=""
+    )
+
+    return parser.parse_args()
+
+NUCS = list("ACTG")
+NUC_MAP = {"A": "T", "T": "A", "C": "G", "G": "C"}
+
+def revcom(dna):
+    return "".join(list(map(lambda n: NUC_MAP[n], list(dna)))[::-1])
+
+def generate_pam_set(pam):
+    pam_stack = [pam]
+
+    while any(["N" in pam for pam in pam_stack]):
+        pam = pam_stack.pop(0)
+
+        if "N" not in pam:
+            pam_stack.append(pam)
+            continue 
+
+        for nuc in NUCS:
+            pam_stack.append(pam.replace("N", nuc, 1))
+
+    return pam_stack
+
+def find_kmers(pam, k, chrm, forward=False):
+    index = 0
+
+    while True:
+        index = chrm.find(pam, index)
+
+        if index == -1:
+            break 
+
+        if forward:
+            kmer = chrm[index - k:index]
+            position = index - k
+        else:
+            kmer = chrm[index + len(pam):index + k]
+            position = index
+
+        index += 1
+        yield (kmer.upper(), position)
+
+def find_all_kmers(pam, k, chrm):
+    pam_set = generate_pam_set(pam)
+    rev_pam_set = list(map(revcom, pam_set))
+
+    for p in pam_set:
+        for kmer, pos in find_kmers(p, k, chrm):
+            yield {"sequence" : kmer, "position" : pos, "pam" : pam, "sense": "+"}
+
+    for p in rev_pam_set:
+        for kmer, pos in find_kmers(p, k, chrm, forward=False):
+            yield {"sequence" : revcom(kmer), "position" : pos,
+                   "pam" : pam, "sense": "-"}
+
+def output_kmer(prefix, chrm_name, kmer):
+    identifier = f"{prefix}{chrm_name}:{kmer['position']}:{kmer['sense']}"
+    row = [identifier, str(kmer['sequence']),
+           kmer['pam'], chrm_name,
+           str(kmer['position']), kmer['sense']]
+    print(",".join(row))
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+
+    print("id,sequence,pam,chromosome,position,sense")
+    for record in SeqIO.parse(args.fasta, "fasta"):
+        if len(record) < args.min_chr_length:
+            continue
+
+        for kmer in find_all_kmers(args.pam, args.kmer_length, record.seq):
+            output_kmer(args.prefix, record.name, kmer)
