ca582764343161f84bbfb402dbace739ba25dbe4
Vineet Bansal <vineetbansal@protonmail.com>
2022-11-07 14:53:48 -0500

    Removed binary building on Windows for now

--- include/version.hpp
+++ include/version.hpp
@@ -1,9 +1,3 @@
-/**
-This file is replaced by an auto-generated version.h
-with an GUIDESCAN_VERSION obtained from a variable supplied
-to cmake
-*/
-
 #ifndef GUIDESCAN_VERSION
-#define GUIDESCAN_VERSION "2.0.0"
+#define GUIDESCAN_VERSION "0.0.0"
 #endif
