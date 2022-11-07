789f4252f5afecc62d989c20a0a60bf8664ca12b
Vineet Bansal <vineetbansal@protonmail.com>
2022-11-07 14:47:44 -0500

    Guidescan versioning and release process

---
+++ include/version.hpp
@@ -0,0 +1,9 @@
+/**
+This file is replaced by an auto-generated version.h
+with an GUIDESCAN_VERSION obtained from a variable supplied
+to cmake
+*/
+
+#ifndef GUIDESCAN_VERSION
+#define GUIDESCAN_VERSION "2.0.0"
+#endif
